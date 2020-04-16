582c024e7b58f8b9f387524b44a8bd37a488a3fa
gatherheart <imonit@naver.com>
2020-04-17 01:20:33 +0900

    [ADD] Simple Graphql Server

---
+++ server.ts
@@ -0,0 +1,39 @@
+import { GraphQLServer } from "graphql-yoga";
+import { prisma } from "./generated/prisma-client";
+// ... or using `require()`
+
+const typeDefs = `
+  type Message {
+    id: ID!
+    text: String!
+  }
+  type Query {
+      messages: [Message!]!
+  }
+  type Mutation {
+      sendMessage(text:String!): Message!
+  }
+  type Subscription {
+      newMessage: Message!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    messages: () => prisma.messages(),
+  },
+
+  Mutation: {
+    sendMessage: (_, { text }) => prisma.createMessage({ text }),
+  },
+
+  Subscription: {
+    newMessage: {
+      subscribe: () => prisma.$subscribe.message().node(),
+      resolve: (payload) => payload,
+    },
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
