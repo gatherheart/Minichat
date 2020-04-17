6161cc0714894a63bd767ae5d0a0c80ba8ac3033
gatherheart <imonit@naver.com>
2020-04-17 22:12:09 +0900

    [ADD] Query & Mutation with refetching queries

--- Chat.js
+++ Chat.js
@@ -1,12 +1,27 @@
-import React from "react";
-import { View, Text, ScrollView, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
 import gql from "graphql-tag";
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import WithSuspense from "./WithSuspense";
 
 const GET_MESSAGES = gql`
-  query messages {
+  query getMessages {
     messages {
+      id
+      text
+    }
+  }
+`;
+
+const SEND_MESSAGE = gql`
+  mutation sendMessage($text: String!) {
+    sendMessage(text: $text) {
       id
       text
     }
@@ -15,23 +30,69 @@ const GET_MESSAGES = gql`
 
 function Chat() {
   const { data, error, loading } = useQuery(GET_MESSAGES);
+  const [message, setMessage] = useState("");
+  const [sendMessageMutation, { data: data_m }] = useMutation(SEND_MESSAGE);
   console.log(data, loading);
+  console.log(data_m);
+
+  const onChangeText = (text) => {
+    console.log(text);
+    setMessage(text);
+  };
+
+  const onSubmit = async () => {
+    if (message === "") return;
+
+    try {
+      await sendMessageMutation({
+        variables: { text: message },
+        refetchQueries: () => [{ query: GET_MESSAGES }],
+      });
+      setMessage("");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   if (loading) return <WithSuspense />;
   return (
-    <ScrollView
-      contentContainerStyle={{
-        paddingVertical: 50,
-        flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "center",
-      }}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
     >
-      {data.messages.map((m) => (
-        <View key={m.id} style={{ marginBottom: 10 }}>
-          <Text>{m.text}</Text>
-        </View>
-      ))}
-    </ScrollView>
+      <ScrollView
+        contentContainerStyle={{
+          paddingVertical: 50,
+          flex: 1,
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        {data.messages.map((m) => (
+          <View key={m.id} style={{ marginBottom: 10 }}>
+            <Text>{m.text}</Text>
+          </View>
+        ))}
+
+        <TextInput
+          style={{
+            marginTop: 50,
+            width: "90%",
+            borderRadius: 10,
+            paddingVertical: 15,
+            paddingHorizontal: 10,
+            backgroundColor: "#f2f2f2",
+          }}
+          onChangeText={onChangeText}
+          onSubmitEditing={onSubmit}
+          value={message}
+          autoCapitalize={"none"}
+          autoCorrect={false}
+          placeholder={"Type a message"}
+          returnKeyType={"send"}
+        ></TextInput>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
