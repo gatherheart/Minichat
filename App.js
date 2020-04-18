8a408c49a17fbbe7ddfcd798cc4329a61c9217ca
gatherheart <imonit@naver.com>
2020-04-18 13:02:25 +0900

    [ADD] Android Push Notification Channel

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
 import { ApolloProvider } from "@apollo/react-hooks";
 import * as Permissons from "expo-permissions";
 import { Notifications } from "expo";
@@ -12,8 +12,20 @@ export default function App() {
     const { status } = await Permissons.askAsync(Permissons.NOTIFICATIONS);
     setNoticeStatus(status);
     let token = await Notifications.getExpoPushTokenAsync();
+
     console.log(token);
   };
+
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      Notifications.createChannelAndroidAsync("chat-messages", {
+        name: "Messages",
+        priority: "max",
+        sound: true,
+        vibrate: [0, 250, 500, 250],
+      });
+    }
+  });
 
   useMemo(() => {
     ask();
